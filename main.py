00cc8cc0c85c08693e3abf5dba7f3e02031393dd
lvdongyi <lvdongyi1222@gmail.com>
2021-10-13 21:29:02 +0800

    2021/10/13 新增表单项fieldBByc1的数据

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import requests,re,json,datetime,os
+import requests,re,json,datetime,os,random
 from lxml import etree
 desp_text=""
 proxies={
-        "http":None,
-        "https":None
+        "http":"http://127.0.0.1:8888",
+        "https":"https://127.0.0.1:8888"
     }
 stulist= {}
 retry=[]
+
 desp_md='''
 | 账号       | 报备结果 | 返回信息 | 表单地址 |
 | ---------- | -------- | -------- | -------- |'''
@@ -30,7 +31,10 @@ while a <len(stu)-1:
         print("分割账号密码失败，宁的Secrets格式确实不对")
         continue
     try:
-        xiegang="\\"
+        danyinghao="\\"
+
+        shit="{"+danyinghao+"_parent"+danyinghao+":"+danyinghao+"230000"+danyinghao+"}"
+        print(shit)
         requests.packages.urllib3.disable_warnings()
         headers={
             "Host": "cas-443.wvpn.hrbeu.edu.cn",
@@ -63,6 +67,7 @@ while a <len(stu)-1:
         body["execution"]=execution[0]
 
         q=req.post("https://cas-443.wvpn.hrbeu.edu.cn/cas/login",proxies=proxies,data=body,headers=headers,verify=False)
+
         if str(q.headers).find("CASTGC")==-1:#未登录成功则输出信息并尝试下一个账号
             print(stulist[a]["STUID"] + " "+stulist[a]["PASSWORD"] + "登录失败")
             desp_md += "\n|" + stulist[a][
@@ -119,7 +124,11 @@ while a <len(stu)-1:
         }
         req.get("https://one.wvpn.hrbeu.edu.cn/infoplus/alive",data=body,proxies=proxies,verify=False)#这个貌似没用，但是抓包时看到有这样一个请求了，也有不一样的返回值
         req.headers['Referer']=formAddress
-        idontknowwhatsthis=json.loads(req.post("https://one.wvpn.hrbeu.edu.cn/infoplus/interface/render",data=body,proxies=proxies,verify=False).text)["entities"][0]["data"]
+        p=json.loads(req.post("https://one.wvpn.hrbeu.edu.cn/infoplus/interface/render",data=body,proxies=proxies,verify=False).text)["entities"][0]
+        instanceId=p["step"]["instanceId"]
+        entryId=p["step"]["entryId"]
+        idontknowwhatsthis=p["data"]
+        userId=p["userId"]
         #我也不知道这是啥东西，反正就是返回了好长一坨信息，还把我的自动缩进卡死了...不过这些信息下面正好可以用到。
         print(idontknowwhatsthis)
         fieldLYyc1=datetime.datetime.now().timestamp()#获取现在的时间
@@ -154,10 +163,10 @@ while a <len(stu)-1:
                "_VAR_STEP_NUMBER":str(idontknowwhatsthis["_VAR_STEP_NUMBER"]),
                "_VAR_POSITIONS":str(idontknowwhatsthis["_VAR_POSITIONS"]),
                "_VAR_OWNER_ORGANIZES_Names":str(idontknowwhatsthis["_VAR_OWNER_ORGANIZES_Names"]),
-               "_VAR_URL":str(idontknowwhatsthis["_VAR_URL"]),
+               "_VAR_URL":formAddress,
                "_VAR_EXECUTE_ORGANIZE_Name":str(idontknowwhatsthis["_VAR_EXECUTE_ORGANIZE_Name"]),
                "_VAR_EXECUTE_INDEP_ORGANIZES_Codes":str(idontknowwhatsthis["_VAR_EXECUTE_INDEP_ORGANIZES_Codes"]),
-               "_VAR_RELEASE":str(idontknowwhatsthis["_VAR_RELEASE"]),
+               "_VAR_RELEASE":"true",
                "_VAR_EXECUTE_POSITIONS":str(idontknowwhatsthis["_VAR_EXECUTE_POSITIONS"]),
                "_VAR_TODAY":str(idontknowwhatsthis["_VAR_TODAY"]),
                "_VAR_NOW_MONTH":str(idontknowwhatsthis["_VAR_NOW_MONTH"]),
@@ -170,6 +179,9 @@ while a <len(stu)-1:
                "groupGYLList":[0],
                "groupQTLList":[0],
                "groupZZLList":[0],
+              "_VAR_URL_Name": formAddress,
+              "_VAR_ENTRY_NAME": "__进出校报备及审批",
+              "_VAR_ENTRY_TAGS": "null",
                "fieldJBsqr":stulist[a]["STUID"],
                "fieldJBsqr_Name":str(idontknowwhatsthis["_VAR_OWNER_REALNAME"]),#申请人
                "fieldJBszxy":str(idontknowwhatsthis["_VAR_OWNER_ORGANIZES_Codes"]),#专业代号，软件工程是12000
@@ -177,30 +189,30 @@ while a <len(stu)-1:
                "fieldJBxh":stulist[a]["STUID"],
                "fieldJBlxfs":"17777777777",#申请人电话
                "fieldJBszgy":"十公寓",#所在公寓
-               "fieldJBfdyxm":"0600010269",#导员账号
-               "fieldJBfdyxm_Name":"王蕾",
+               "fieldJBfdyxm":"0000000001",#导员账号
+               "fieldJBfdyxm_Name":"张三",#导员姓名
                "fieldJBfdylxdh":"17777775492",#导员电话
                "fieldJBjcxlx":"1",#1是报备:-)2是审批
-               "fieldBBh1":"1",#这是个啥？？？
+               "fieldBBh1":"",#这是个啥？？？
                "fieldBByc1":"",
                "fieldBByc2":"",
                "fieldBByc3":"",
-               "fieldLYyc1":str(fieldLYyc1),#当前时间
-               "fieldBBcxsy":"玩",#出校目的
+               "fieldLYyc1":int(str(fieldLYyc1)[:10]),#当前时间
+               "fieldBBcxsy":"出校",#出校目的
                "fieldBBsylb":"7",#出校原因1：因公出校  2：病假  3：事假  4：求职  5：实习  6：返家  7：其他
                "fieldBBycsj":"",
-               "fieldBBcxrqFrom":str(fieldBBcxrqFrom).split(".")[0],#授权开始日期
-               "fieldBBcxsjFrom":str(fieldBBcxsjFrom).split(".")[0],#授权开始时间（现在的时间戳-今天0点的）
-               "fieldBBcxrqTo":str(fieldBBcxrqTo).split(".")[0],#授权结束时间戳
-               "fieldBBcxsjTo":str(fieldBBcxsjTo).split(".")[0],#授权结束时间（结束的时间戳-今天0点的）
+               "fieldBBcxrqFrom":int(str(fieldBBcxrqFrom).split(".")[0]),#授权开始日期
+               "fieldBBcxsjFrom":int(str(fieldBBcxsjFrom).split(".")[0]),#授权开始时间（现在的时间戳-今天0点的）
+               "fieldBBcxrqTo":int(str(fieldBBcxrqTo).split(".")[0]),#授权结束时间戳
+               "fieldBBcxsjTo":int(str(fieldBBcxsjTo).split(".")[0]),#授权结束时间（结束的时间戳-今天0点的）
                "fieldBBsheng":"230000",#省邮编
                "fieldBBsheng_Name":"黑龙江省",
                "fieldBBshi":"230100",#市邮编
                "fieldBBshi_Name":"哈尔滨市",
-               "fieldBBshi_Attr":"{"+xiegang+"_parent"+xiegang+":"+xiegang+"230000"+xiegang+"}",
+               "fieldBBshi_Attr":str(shit),
                "fieldBBdiqu":"230103",#区邮编 Shit...为啥要分别提交三个邮编
                "fieldBBdiqu_Name":"南岗区",
-               "fieldBBdiqu_Attr":"{"+xiegang+"_parent"+xiegang+":"+xiegang+"230000"+xiegang+"}",
+               "fieldBBdiqu_Attr":str(shit),
                "fieldBBxxdz":"南通大街",#详细地址
                "fieldLYyc":"",
                "fieldJLly":"",
@@ -214,7 +226,7 @@ while a <len(stu)-1:
                "fieldGYLmc_Name":[""],
                "fieldGYLfjh":[""],
                "fieldLYgyyc":"",
-               "fieldLYqtl":[""],
+               "fieldLYqtl":"false",
                "fieldQTLmc":[""],
                "fieldQTLmc_Name":[""],
                "fieldQTLfjh":[""],
@@ -236,30 +248,65 @@ while a <len(stu)-1:
                "fieldFSJspyj":"",
                "fieldFSJspr":"",
                "fieldFSJspsj":"",
-               "_VAR_URL_Name":formAddress,
-               "_VAR_ENTRY_NAME":"__进出校报备及审批",
-               "_VAR_ENTRY_TAGS":"null"
                }
+
+        #临时加的表单项,浪费我好久时间。。。。
+        body_tmp={
+            "fieldName":"fieldJBjcxlx",
+            "fieldValue":"1",
+            "path":"",
+            "boundFields":"fieldFDYsprq,fieldJBszgy,fieldBBshi,fieldLYqtl,fieldBBsheng,fieldLYqtyc,fieldLYzzl,fieldDel,fieldJBszxy,fieldFSJspsj,fieldLYrqFrom,fieldLYgyl,fieldGYLfjh,fieldFSJspr,fieldQTLmc,fieldLYbgl,fieldBBcxrqFrom,fieldJBjcxlx,fieldJLly,fieldLYzzyc,fieldBBsylb,fieldFDYspyj,fieldLYyc,fieldZMCLUrl,fieldBBcxsjFrom,fieldBBycsj,fieldBBdiqu,fieldJBsqr,fieldLYsjTo,fieldJBlxfs,fieldJBxh,fieldBBcxsy,fieldSPyc,fieldLYyc1,fieldZZLfjh,fieldBBcxsjTo,fieldBGLfjh,fieldFSJspyj,fieldLYrqTo,fieldBByc2,fieldBByc3,fieldQTLfjh,fieldBByc1,fieldJBfdyxm,fieldBBh1,fieldBBcxrqTo,fieldLYbgyc,fieldZZLmc,fieldLYgyyc,fieldLYsjFrom,fieldGYLmc,fieldBBxxdz,fieldJBfdylxdh,fieldBGLmc,fieldFDYspr",
+            "csrfToken":csrfToken,
+            "lang":"zh",
+            "instanceId":instanceId,
+            "stepId":stedId,
+            "entryId":entryId,
+            "workflowId":"null",
+        }
+        body_tmp["formData"]=str(form).replace(" ","").replace("'","\"").replace("\"false\"","false").replace("\\\\","\\\"")#提交时处理
+        p=json.loads(req.post("https://one.wvpn.hrbeu.edu.cn/infoplus/interface/fieldChanging",data=body_tmp,proxies=proxies,verify=False).text)
+        fieldBByc1=json.loads(p["entities"][0])["bByc1"]
+        form["fieldBByc1"]=fieldBByc1
         body["actionId"]=1
-        body["formData"]=str(form).replace("'","\"")#提交时把字典中的单引号换成双引号
-        body["boundFields"]: "fieldFDYsprq,fieldJBszgy,fieldBBshi,fieldLYqtl,fieldBBsheng,fieldLYqtyc,fieldLYzzl,fieldDel,fieldJBszxy,fieldFSJspsj,fieldLYrqFrom,fieldLYgyl,fieldGYLfjh,fieldFSJspr,fieldQTLmc,fieldLYbgl,fieldBBcxrqFrom,fieldJBjcxlx,fieldJLly,fieldLYzzyc,fieldBBsylb,fieldFDYspyj,fieldLYyc,fieldZMCLUrl,fieldBBcxsjFrom,fieldBBycsj,fieldBBdiqu,fieldJBsqr,fieldLYsjTo,fieldJBlxfs,fieldJBxh,fieldBBcxsy,fieldSPyc,fieldLYyc1,fieldZZLfjh,fieldBBcxsjTo,fieldBGLfjh,fieldFSJspyj,fieldLYrqTo,fieldBByc2,fieldBByc3,fieldQTLfjh,fieldBByc1,fieldJBfdyxm,fieldBBh1,fieldBBcxrqTo,fieldLYbgyc,fieldZZLmc,fieldLYgyyc,fieldLYsjFrom,fieldGYLmc,fieldBBxxdz,fieldJBfdylxdh,fieldBGLmc,fieldFDYspr"
+        body["formData"]=str(form).replace(" ","").replace("'","\"").replace("\"false\"","false").replace("\\\\","\\\"")#提交时处理
+        body["boundFields"]= "fieldFDYsprq,fieldJBszgy,fieldBBshi,fieldLYqtl,fieldBBsheng,fieldLYqtyc,fieldLYzzl,fieldDel,fieldJBszxy,fieldFSJspsj,fieldLYrqFrom,fieldLYgyl,fieldGYLfjh,fieldFSJspr,fieldQTLmc,fieldLYbgl,fieldBBcxrqFrom,fieldJBjcxlx,fieldJLly,fieldLYzzyc,fieldBBsylb,fieldFDYspyj,fieldLYyc,fieldZMCLUrl,fieldBBcxsjFrom,fieldBBycsj,fieldBBdiqu,fieldJBsqr,fieldLYsjTo,fieldJBlxfs,fieldJBxh,fieldBBcxsy,fieldSPyc,fieldLYyc1,fieldZZLfjh,fieldBBcxsjTo,fieldBGLfjh,fieldFSJspyj,fieldLYrqTo,fieldBByc2,fieldBByc3,fieldQTLfjh,fieldBByc1,fieldJBfdyxm,fieldBBh1,fieldBBcxrqTo,fieldLYbgyc,fieldZZLmc,fieldLYgyyc,fieldLYsjFrom,fieldGYLmc,fieldBBxxdz,fieldJBfdylxdh,fieldBGLmc,fieldFDYspr"
         #print(body["formData"])
-        results=req.post("https://one.wvpn.hrbeu.edu.cn/infoplus/interface/listNextStepsUsers",data=body,proxies=proxies,verify=False)#提交
+        body["rand"]=str(random.random()*10e3)[:3]+"."+str(random.random()*10e14)[:14]
+        body["timestamp"] = int(str(datetime.datetime.now().timestamp())[:10])
+        #print(str(body))
+        results=req.post("https://one.wvpn.hrbeu.edu.cn/infoplus/interface/listNextStepsUsers",data=body,proxies=proxies,verify=False)#提交流程1，用于验证表单数据
+        body["nextUsers"]="{}"
+        body["remark"]=""
+        body["rand"] = str(random.random() * 10e3)[:3] + "." + str(random.random() * 10e14)[:14]
         result=json.loads(results.text)
         print(result)
-        if result["errno"]==0:
-            #报备成功
-            desp_md+="\n|"+stulist[a]["STUID"]+"|"+"成功"+"|报备时间："+datetime.datetime.fromtimestamp(fieldLYyc1+120).strftime("%Y-%m-%d %H:%M:%S")+\
-                datetime.datetime.fromtimestamp(fieldBBcxrqFrom).strftime("%Y-%m-%d")+"22:00:00"+"    报备结果："+str(result)+"|"+formAddress+"|"
-            desp_text+=stulist[a]["STUID"]+"报备成功，报备时间为"+datetime.datetime.fromtimestamp(fieldLYyc1+120).strftime("%Y-%m-%d %H:%M:%S")+\
-                datetime.datetime.fromtimestamp(fieldBBcxrqFrom).strftime("%Y-%m-%d")+"22:00:00"+"表单地址："+formAddress+"\n\n"
-            pass
+        #"userId":""
+        if result["errno"]!=6:
+            result = req.post("https://one.wvpn.hrbeu.edu.cn/infoplus/interface/doAction", data=body, proxies=proxies,
+                               verify=False)#正式提交表单
+            print(result.text)
+            if result["errno"]==0:
+                # 报备成功
+                desp_md += "\n|" + stulist[a]["STUID"] + "|" + "成功" + "|报备时间：" + datetime.datetime.fromtimestamp(fieldLYyc1 + 120).strftime("%Y-%m-%d %H:%M:%S") +"  到  "+ \
+                        datetime.datetime.fromtimestamp(fieldBBcxrqFrom).strftime(
+                            "%Y-%m-%d") + "22:00:00" + "    报备结果：" + str(result) + "|" + formAddress + "|"
+                desp_text += stulist[a]["STUID"] + "报备成功，报备时间为" + datetime.datetime.fromtimestamp(fieldLYyc1 + 120).strftime(
+                            "%Y-%m-%d %H:%M:%S") + \
+                    datetime.datetime.fromtimestamp(fieldBBcxrqFrom).strftime(
+                        "%Y-%m-%d") + "22:00:00" + "表单地址：" + formAddress + "\n\n"
+                pass
+            else :
+                # 报备失败
+                desp_md += "\n|" + stulist[a]["STUID"] + "|" + "失败" + "|" + str(result) + "|" + formAddress + "|"
+                desp_text += stulist[a]["STUID"] + "报备失败，返回结果为" + str(result) + "表单地址：" + formAddress + "\n\n"
+                pass
+
         else :
             #报备失败
             desp_md+="\n|"+stulist[a]["STUID"]+"|"+"失败"+"|"+str(result)+"|"+formAddress+"|"
             desp_text+=stulist[a]["STUID"]+"报备失败，返回结果为"+str(result)+"表单地址："+formAddress+"\n\n"
             pass
-    except Exception as e:
+    except TypeError as e:
         print(stulist[a]["STUID"]+"报备失败，程序可能出错了："+str(e))
         desp_md+="\n|"+stulist[a]["STUID"]+"|"+"失败"+"|"+str(e)+"|"+"|"
         desp_text+=stulist[a]["STUID"]+"报备失败，有可能是程序出错了："+str(e)+"\n\n"
